a7e3792f0894604f42be5a9e6c14c87ad3c2991d
bhanujggandhi <gandhibhanuj@gmail.com>
2020-03-26 14:39:29 +0530

    Create table object

--- script.py
+++ script.py
@@ -30,3 +30,16 @@ for row in all_rows:
 stats[-1][1] = "Total Cases"
 
 stats.remove(stats[-1])
+
+objects = []
+for row in stats:
+    objects.append(row[1])
+
+y_pos = np.arange(len(objects))
+
+performance = []
+for row in stats:
+    performance.append(int(row[2]) + int(row[3]))
+
+table = tabulate(stats, headers = SHORT_HEADERS)
+print(table)
