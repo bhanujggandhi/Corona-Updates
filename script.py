bb90101c9c18642774a2abf123ce020c99286b4c
bhanujggandhi <gandhibhanuj@gmail.com>
2020-03-26 14:34:59 +0530

    Create BeautifulSoup module

--- script.py
+++ script.py
@@ -4,3 +4,29 @@ from tabulate import tabulate
 import os
 import numpy as np
 import matplotlib.pyplot as plt
+
+extract_contents = lambda row: [x.text.replace('\n', '') for x in row]
+URL = 'https://www.mohfw.gov.in/'
+
+SHORT_HEADERS = ['Sno', 'State', 'Indian-Confirmed', 'Foreign-Confirmed', 'Cured', 'Death']
+
+response = requests.get(URL).content
+soup = BeautifulSoup(response, 'html.parser')
+header = extract_contents(soup.tr.find_all('th'))
+
+stats = []
+all_rows = soup.find_all('tr')
+
+for row in all_rows:
+    stat = extract_contents(row.find_all('td'))
+    if stat:
+        if len(stat) == 5:
+            stat = ['', *stat]
+            stats.append(stat)
+
+        elif len(stat) == 6:
+            stats.append(stat)
+
+stats[-1][1] = "Total Cases"
+
+stats.remove(stats[-1])
