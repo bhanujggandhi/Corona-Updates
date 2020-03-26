674f187da4bf4fcbbcef19e203dca0cc04e74717
bhanujggandhi <gandhibhanuj@gmail.com>
2020-03-26 14:55:49 +0530

    Create Plot

--- script.py
+++ script.py
@@ -43,3 +43,13 @@ for row in stats:
 
 table = tabulate(stats, headers = SHORT_HEADERS)
 print(table)
+
+plt.barh(y_pos, performance, align='center', alpha=0.5,
+            color=(234/256.0, 128/256.0, 252/256.0), 
+            edgecolor=(106/256.0, 27/256.0, 154/256.0))
+
+plt.yticks(y_pos, objects)
+plt.xlim(1,80)
+plt.xlabel('Number of cases')
+plt.title('Corona Virus Cases')
+plt.show()
